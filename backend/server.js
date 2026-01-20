bcfec2c5537c4db950983b468d274befae6ab56d
hababdu <habibullox000@icloud.com>
2026-01-21 01:06:04 +0500

    jh

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,8 @@ const wss = new WebSocket.Server({ server });
 const PORT = process.env.PORT || 10000;
 const MONGODB_URI = process.env.MONGODB_URI;
 const BOT_TOKEN = process.env.BOT_TOKEN;
-const WEB_APP_URL = process.env.WEB_APP_URL || 'https://your-mini-app-domain.com';
-const ADMIN_TOKEN = process.env.ADMIN_TOKEN || 'admin-secret-key';
+const WEB_APP_URL = process.env.WEB_APP_URL ;
+const ADMIN_TOKEN = process.env.ADMIN_TOKEN ;
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
 // Timeout konfiguratsiyalari
