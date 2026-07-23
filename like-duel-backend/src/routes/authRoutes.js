7f312eda4bc76315358492282906d7c000513b8f
Habibullo Abdumutallibov <habibulloabdumutallibov@MacBook-Air-Habibullo.local>
2026-07-23 13:25:13 +0600

    bot2

--- like-duel-backend/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/authRoutes.js
-import express from 'express';
-import { authController } from '../controllers/authController.js';
-
-const router = express.Router();
-
-router.post('/', authController.auth);
-
-export default router;
